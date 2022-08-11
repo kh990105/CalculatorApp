acf92e6323a10eee3ea432e91a4f5f7c9382ebc7
hyunji0430 <hjdkdlel0430@gmail.com>
2022-08-11 16:42:11 +0900

    setting

---
+++ components/Display.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { View, Text, StyleSheet, Button } from 'react-native'
+
+const Display = () => {
+  return (
+    <div>Display</div>
+  )
+}
+
+
+
+const light = StyleSheet.create({
+
+});
+
+export default Display
